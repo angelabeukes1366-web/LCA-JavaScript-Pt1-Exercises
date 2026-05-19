ec6cfd100ce91c660558ee5324c9244c5f577730
Angela Beukes <angelabeukes1366@gmail.com>
2026-05-20 01:31:45 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,15 @@
+let number1 = 10;
+
+let number2 = 5;
+
+let number1String = String(number1);
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("Number 1:", number1);
+console.log("Number 2:", number2);
+console.log("Number 1 as a string:", number1String);
+console.log("Sum:", sum);
+console.log("Product:", product);
